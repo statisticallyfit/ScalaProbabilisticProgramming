1405f6a62dc7ce2bf9370275e71aba71371701be
BabitzdaBrave <amg.vintila@gmail.com>
2020-06-30 15:10:08 -0400

    added sequence of hidden states function

--- src/main/scala/HiddenMarkovModel/HMMWeatherExample/HMMInfo.scala
+++ src/main/scala/HiddenMarkovModel/HMMWeatherExample/HMMInfo.scala
@@ -21,12 +21,28 @@ object HMMInfo {
 
 	/**
 	 * TODO Takes conditional probabilities which define the transition matrix as input
+	 * Given the rows of the transition matrix, this produces a function of type Element[H] => Element[H]
 	 */
 	def transition(rainyTransition: Array[Double], sunnyTransition: Array[Double])(e: Element[H]): Element[H] = {
 
+
+		/**
+		 * Gives the distribution over hidden states at time t given the state at time (t-1)
+		 * https://hyp.is/NweblroCEeq29IOl4FccPA/mioalter.wordpress.com/2016/02/13/hmm-hidden-markov-models-with-figaro/
+		 * @param h
+		 * @return
+		 */
 		def transitionGivenState(h: H) : Element[H] = h match {
-			case Rainy => Select(rainyTransition(0) -> Rainy, rainyTransition(1) -> Sunny)
-			case Sunny => Select(sunnyTransition(0) -> Rainy, sunnyTransition(1) -> Sunny)
+			case Rainy =>
+				Select(
+					rainyTransition(0) -> Rainy,
+					rainyTransition(1) -> Sunny
+				)
+			case Sunny =>
+				Select(
+					sunnyTransition(0) -> Rainy,
+					sunnyTransition(1) -> Sunny
+				)
 		}
 
 		e.flatMap(h => transitionGivenState(h))
@@ -42,17 +58,59 @@ object HMMInfo {
 
 		def emissionGivenState(h: H) : Element[O] = h match {
 			case Rainy =>
-				Select(rainyEmission(0) -> Shop,
+				Select(
+					rainyEmission(0) -> Shop,
 					rainyEmission(1) -> Clean,
-					rainyEmission(2) -> Walk)
+					rainyEmission(2) -> Walk
+				)
 
 			case Sunny =>
-				Select(sunnyEmission(0) -> Shop,
+				Select(
+					sunnyEmission(0) -> Shop,
 					sunnyEmission(1) -> Clean,
-					sunnyEmission(2) -> Walk)
+					sunnyEmission(2) -> Walk
+				)
 		}
 
 		e.flatMap(h => emissionGivenState(h))
 
 	}
+
+
+	/**
+	 *
+	 * Makes a sequence of hidden states in which each hidden state is obtained from
+	 * the previous one by applying the transition matrix.
+	 *
+	 * @param length
+	 * @param transitionMatrix
+	 * @param aprioriArray
+	 * @return
+	 */
+	def makeHiddenSequence(length: Int, transitionMatrix: Element[H] => Element[H],
+					   aprioriArray: Array[Double]) : List[Element[H]] = {
+
+
+		//Creating the apriori distribution over hidden states.
+		val aprioriDist = Select(
+			aprioriArray(0) -> Rainy,
+			aprioriArray(1) -> Sunny
+		)
+
+
+		def buildSequence(n: Int, elems: List[Element[H]]): List[Element[H]] = {
+			(n, elems) match {
+				// Step 3: when we get down to n = 0, we are done
+				case (0, elems) => elems
+				// Step 1: start with the apriori distribution
+				case (n, Nil) => buildSequence(n - 1, List(aprioriDist))
+				// Step 2: apply the transitionMatrix argument to the head and attach it onto the list
+				case (n, x :: xs) => buildSequence(n - 1, transitionMatrix(x) :: x :: xs)
+			}
+		}
+
+		// reverse the result of hidden states so time increases from left to right
+		buildSequence(length, Nil).reverse 
+	}
+
 }
